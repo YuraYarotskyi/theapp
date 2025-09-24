d9b8fce624089c9597283cd94b8fb3bc3c25d7eb
YuraYarotskyi <105173686+YuraYarotskyi@users.noreply.github.com>
2025-09-24 14:52:04 +0200

    Aktualisieren von startupfile.sh

--- startupfile.sh
+++ startupfile.sh
@@ -2,6 +2,13 @@ sudo apt install openssh-server
 sudo systemctl start ssh
 sudo cp /lib/systemd/system/ssh.service /etc/systemd/system/myserver.service
 
+
+
+# Change the Port line (uncomment if needed, replace any existing Port line)
+sudo sed -i "/^#Port /c\Port 22" /etc/ssh/sshd_config
+sudo sed -i "/^Port /c\Port 22" /etc/ssh/sshd_config
+
+
 sudo systemctl daemon-reload
 sudo systemctl disable ssh.service
 sudo systemctl enable myserver.service
