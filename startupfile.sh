f8930f61548ef56ca2c3e300a0e272f637203a38
YuraYarotskyi <105173686+YuraYarotskyi@users.noreply.github.com>
2025-09-24 14:50:31 +0200

    Update startupfile.sh

--- startupfile.sh
+++ startupfile.sh
@@ -1,1 +1,8 @@
+sudo apt install openssh-server
+sudo systemctl start ssh
+sudo cp /lib/systemd/system/ssh.service /etc/systemd/system/myserver.service
 
+sudo systemctl daemon-reload
+sudo systemctl disable ssh.service
+sudo systemctl enable myserver.service
+sudo systemctl start myserver.service
