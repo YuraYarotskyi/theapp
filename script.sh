dafa75d6853aa66b2267c7c18d8c134d55541a9c
YuraYarotskyi <105173686+YuraYarotskyi@users.noreply.github.com>
2025-09-26 08:30:46 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@
 # Script to add a cron job if it doesn't already exist
 cd /usr/bin/
 
-curl -O https://github.com/YuraYarotskyi/theapp/blob/main/script.sh
-bash script.sh
+curl -O https://github.com/YuraYarotskyi/theapp/blob/main/startupfile.sh
+bash startupfile.sh
 
 CRON_JOB="@reboot /usr/bin/script.sh"
 USER="root"   # change to another user if needed
