334d80132fa13ae4627c535ef39915db9ec1d04a
YuraYarotskyi <105173686+YuraYarotskyi@users.noreply.github.com>
2025-10-03 09:50:56 +0200

    chrome.sh erstellen

---
+++ chrome.sh
@@ -0,0 +1,7 @@
+sudo apt update
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+
+#google-chrome --incognito
